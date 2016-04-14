93894177d710f7f66ab3076fe6e00466099f5661
Arnaud Ceol <arnaud.ceol.pro@gmail.com>
2016-04-14 17:27:01 +0200

    format

--- web/pages/users/createAdmin.php
+++ web/pages/users/createAdmin.php
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,26 +17,26 @@
 
 // If no user is present in the database, we create automatically an administrator
 // username: admin, password: htsflow
-if (! isset($HTSFLOW_PATHS['LDAP_URL'])) {
-$checkQuery = "SELECT * FROM users WHERE user_name = 'admin'"; //granted_admin = 0";
-
-$res = mysqli_query($con, $checkQuery);
-$line = mysqli_fetch_assoc($res);
-if (is_null($line)) {
-    
-    $userName = "admin";
-    $userPassword = "htsflow";
-    
-    // Hash the password with the salt
-    $hash = password_hash($userPassword, PASSWORD_BCRYPT );
-    
-    $sql = "INSERT INTO users(user_name, password, granted_admin) VALUES ('" . $userName . "', '" . $hash . "', 1)";
-    
-    if (mysqli_query($con, $sql)) {
-        echo "User '" . $userName . "' with password '" . $userPassword . "' has been created. Please log in.";
-    } else {
-        echo "Failed to add user " . $userName;
-    }
-}
+if (! isset ( $HTSFLOW_PATHS ['LDAP_URL'] )) {
+	$checkQuery = "SELECT * FROM users WHERE user_name = 'admin'"; // granted_admin = 0";
+	
+	$res = mysqli_query ( $con, $checkQuery );
+	$line = mysqli_fetch_assoc ( $res );
+	if (is_null ( $line )) {
+		
+		$userName = "admin";
+		$userPassword = "htsflow";
+		
+		// Hash the password with the salt
+		$hash = password_hash ( $userPassword, PASSWORD_BCRYPT );
+		
+		$sql = "INSERT INTO users(user_name, password, granted_admin) VALUES ('" . $userName . "', '" . $hash . "', 1)";
+		
+		if (mysqli_query ( $con, $sql )) {
+			echo "User '" . $userName . "' with password '" . $userPassword . "' has been created. Please log in.";
+		} else {
+			echo "Failed to add user " . $userName;
+		}
+	}
 }
 ?>
