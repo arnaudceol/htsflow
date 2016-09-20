bacc8f0e96f2d0cf36bf754cacac268c369289f8
Arnaud Ceol <arnaud.ceol.pro@gmail.com>
2016-09-20 14:29:51 +0200

    load stranded rna seq alignment in IGB

--- web/igb.php
+++ web/igb.php
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,75 +14,79 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-session_start();
+session_start ();
 
 require_once ("config.php");
 require ('pages/dbaccess.php');
 
-header("Content-type: text/txt");
+header ( "Content-type: text/txt" );
 
-$files = array();
+$files = array ();
 
-
-if (strpos($_GET['id'], 'primary')) {
+if (strpos ( $_GET ['id'], 'primary' )) {
 	$analysisType = 'primary';
-	$analysisId = str_replace('/primary.igb', '', $_GET['id']);
-	$queryGenome = sprintf("SELECT ref_genome FROM sample, primary_analysis WHERE sample_id = sample.id AND primary_analysis.id = '%s'", $analysisId);
-	$files[] = "/primary/tracks/bw/" .  $analysisId . ".bw";
-	$resGenome = mysqli_query($con, $queryGenome);
-	$htsFlowGenome =  mysqli_fetch_assoc($resGenome)["ref_genome"];	
-	mysqli_free_result($resGenome);
-} else {
-	preg_match('/([0-9]+)\/([a-z\_]+)\.igb/',$_GET['id'],$m);
-	$analysisId = $m[1];
-	$analysisType = $m[2];
-	$queryGenome = sprintf("SELECT ref_genome, label FROM sample, primary_analysis, $analysisType WHERE sample_id = sample.id  AND primary_id = primary_analysis.id AND secondary_id = %s", $analysisId);
-
-	$igbQuery = mysqli_query($con, $queryGenome);
-	while($igbResult = mysqli_fetch_assoc($igbQuery)) {
-		$htsFlowGenome = $igbResult["ref_genome"];	
-		$files[] = "/secondary/" .  $analysisId . "/bed/" . $igbResult["label"] . "_peaks.bb";
+	$analysisId = str_replace ( '/primary.igb', '', $_GET ['id'] );
+	$queryGenome = sprintf ( "SELECT ref_genome, stranded FROM sample, pa_options, primary_analysis WHERE pa_options.id = options_id AND sample_id = sample.id AND primary_analysis.id = '%s'", $analysisId );
+	$files [] = "/primary/tracks/bw/" . $analysisId . ".bw";
+	$resGenome = mysqli_query ( $con, $queryGenome );
+	$row = mysqli_fetch_assoc ( $resGenome );
+	$htsFlowGenome = $row ["ref_genome"];
+	$stranded = $row ["stranded"];
+	if ($stranded == 1) {
+		$files [] = "/primary/tracks/bw/" . $analysisId . "_p.bw";
+		$files [] = "/primary/tracks/bw/" . $analysisId . "_n.bw";
 	}
-	mysqli_free_result($igbQuery);
+	
+	mysqli_free_result ( $resGenome );
+} else {
+	preg_match ( '/([0-9]+)\/([a-z\_]+)\.igb/', $_GET ['id'], $m );
+	$analysisId = $m [1];
+	$analysisType = $m [2];
+	$queryGenome = sprintf ( "SELECT ref_genome, label FROM sample, primary_analysis, $analysisType WHERE sample_id = sample.id  AND primary_id = primary_analysis.id AND secondary_id = %s", $analysisId );
+	
+	$igbQuery = mysqli_query ( $con, $queryGenome );
+	while ( $igbResult = mysqli_fetch_assoc ( $igbQuery ) ) {
+		$htsFlowGenome = $igbResult ["ref_genome"];
+		$files [] = "/secondary/" . $analysisId . "/bed/" . $igbResult ["label"] . "_peaks.bb";
+	}
+	mysqli_free_result ( $igbQuery );
 }
-// get info from primary 
+// get info from primary
 // Read synonyms:
 $synonymsUrl = "https://bitbucket.org/lorainelab/integrated-genome-browser/raw/1194f9df27bb771cac7422af28c673a46a4d22b4/core/synonym-lookup/src/main/resources/synonyms.txt";
-$fp = fopen($synonymsUrl, 'r');
+$fp = fopen ( $synonymsUrl, 'r' );
 
+$synonymToGenome = array ();
 
+$file = file_get_contents ( $synonymsUrl );
+while ( ! feof ( $fp ) ) {
+	$line = fgets ( $fp, 2048 );
+	$delimiter = "\t";
+	$data = str_getcsv ( $line, $delimiter );
+	$genome = $data [0];
+	foreach ( $data as $synonym ) {
+		$synonymToGenome [$synonym] = $genome;
+	}
+}
 
-$synonymToGenome = array();
-
-$file = file_get_contents($synonymsUrl);
-while ( !feof($fp) )
-{
-    $line = fgets($fp, 2048);
-     $delimiter = "\t";
-     $data = str_getcsv($line, $delimiter);
- 	$genome = $data[0];
-     foreach ( $data as $synonym ) {
-     	$synonymToGenome[$synonym] = $genome;   	
-     }
- }                              
-
- fclose($fp);
- $igbGenome = $synonymToGenome[$htsFlowGenome];
-//$igbGenome =$htsFlowGenome;
-//   http://127.0.0.1:7085/UnibrowControl?scriptfile=http://wiki.transvar.org/confluence/download/attachments/17269487/add_new_data.igb
+fclose ( $fp );
+$igbGenome = $synonymToGenome [$htsFlowGenome];
+// $igbGenome =$htsFlowGenome;
+// http://127.0.0.1:7085/UnibrowControl?scriptfile=http://wiki.transvar.org/confluence/download/attachments/17269487/add_new_data.igb
 // http://127.0.0.1:7085/UnibrowControl?scriptfile=http://localhost:3030/htsflow2/web/igb.php?id=12980
 
- $pageURL = 'http';
- if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
- $pageURL .= "://";
- if ($_SERVER["SERVER_PORT"] != "80") {
- 	$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"];
- } else {
- 	$pageURL .= $_SERVER["SERVER_NAME"];
- }
- 
- 
- ?>
+$pageURL = 'http';
+if (isset ( $_SERVER ["HTTPS"] ) && $_SERVER ["HTTPS"] == "on") {
+	$pageURL .= "s";
+}
+$pageURL .= "://";
+if ($_SERVER ["SERVER_PORT"] != "80") {
+	$pageURL .= $_SERVER ["SERVER_NAME"] . ":" . $_SERVER ["SERVER_PORT"];
+} else {
+	$pageURL .= $_SERVER ["SERVER_NAME"];
+}
+
+?>
 # Scripting and the IGB command language
 # 
 # How to add a new data set
@@ -92,8 +96,10 @@ while ( !feof($fp) )
 # select genome of choice
 genome <?php echo $igbGenome . "\n"; ?>
 # Add a new data set
-<?php  foreach ($files as $file) { 
-echo "load " .$pageURL ."/" . $HTSFLOW_PATHS["HTSFLOW_WEB_TRACKS"] . $file . "\n";
+<?php
+
+foreach ( $files as $file ) {
+	echo "load " . $pageURL . "/" . $HTSFLOW_PATHS ["HTSFLOW_WEB_TRACKS"] . $file . "\n";
 }
 ?>
 
