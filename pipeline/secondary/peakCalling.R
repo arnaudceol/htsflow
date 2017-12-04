97415cd3f1cb3890b45bb373d3baa94849cecbc4
Arnaud Ceol <arnaud.ceol.pro@gmail.com>
2017-12-04 17:30:36 +0100

    add macs2 option in config file

--- pipeline/secondary/peakCalling.R
+++ pipeline/secondary/peakCalling.R
@@ -4,9 +4,9 @@
 # we should choose which version to import.
 #
 
-if (getHTSFlowPath("HTSFLOW_PIPELINE") == "2.0") {
+if (getHTSFlowPath("MACS2_VERSION") == "2.0") {
 	pcPath = "pcmacs2.0"
-} else if (getHTSFlowPath("HTSFLOW_PIPELINE") == "2.1") {
+} else if (getHTSFlowPath("MACS2_VERSION") == "2.1") {
 	pcPath = "pcmacs2.1"
 } else {
 	stop("The version of macs 2 is not specified. Please add MACS2_VERSION, with value 2.0 or 2.1, in the configuration file.")
